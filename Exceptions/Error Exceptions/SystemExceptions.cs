3d9e7f0edf6813241fb8c47a231564d248ff0a3c
hassan5544 <hmh.lol55@gmail.com>
2024-03-19 00:01:22 +0300

    commit by hassan Add Exceptions project has errors for now 2024-03-19

---
+++ Exceptions/Error Exceptions/SystemExceptions.cs
@@ -0,0 +1,25 @@
+﻿using LMS.Domain.Extentions;
+using Logger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions.Error_Exceptions
+{
+    public class SystemExceptions : Exception
+    {
+        private readonly ILoggerManager _loggerManager;
+        public SystemExceptions(ILoggerManager loggerManager , Exception ex) : base(String.Format("System Error "))
+        {
+            _loggerManager = loggerManager;
+            _loggerManager.LogError("Exception : " + ex.Message);
+            if(ex.InnerException != null )
+                _loggerManager.LogError(message: "Inner Exeption :" + ex.InnerException.Message);
+
+        }
+
+
+    }
+}
--- LMS.Domain/Entities/BaseEntity.cs
+++ LMS.Domain/Entities/BaseEntity.cs
@@ -9,14 +9,14 @@ namespace LMS.Domain.Entities
 {
     public class BaseEntity<T>
     {
-        public T? Id { get; set; }
-        public string Name { get; set; }
-        public DateTime CreateDate { get; set; }
-        public int CreateUser { get; set; }
-        public DateTime UpdateDate { get; set; }
-        public int UpdateUser { get; set; }
-        public bool IsDeleted { get; set; }
-        public DateTime DeleteDate { get; set; }
-        public int DeleteUser { get; set; }
+        public T? Id { get; protected set; }
+        public string Name { get; protected set; }
+        public DateTime CreateDate { get; protected set; }
+        public int CreateUser { get; protected set; }
+        public DateTime UpdateDate { get; protected set; }
+        public int UpdateUser { get; protected set; }
+        public bool IsDeleted { get; protected set; }
+        public DateTime DeleteDate { get; protected set; }
+        public int DeleteUser { get; protected set; }
     }
 }
